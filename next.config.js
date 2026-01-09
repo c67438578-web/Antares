cdd495ff394935362c6986534fed1e8a238cc380
c67438578-web <c67438578@gmail.com>
2026-01-09 08:36:17 +0100

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: [
+      'antaresenergia.com',
+      'indigo-goat-999288.hostingersite.com',
+      'images.unsplash.com'
+    ],
+  },
+}
+
+module.exports = nextConfig
